df8aaa44423904296b04dde86744998938ad9201
Kyle Macey <kylemacey@github.com>
2018-08-20 18:30:37 -0400

    Code cleanup

--- app/services/data_request.rb
+++ app/services/data_request.rb
@@ -1,62 +1,35 @@
 require 'net/http'
 
 class DataRequest
-  attr_reader :data
+  API_URL = URI.parse(
+    "http://calendar.libraryweb.org/ajax/calendar/list?c=-1&perpage=100&page=%d&date=0000-00-00"
+  ).freeze
 
   # Set arguments here
-  def initialize(data)
-    @data = data
+  def initialize
   end
 
   # * means capture all arguments and keep them in an array called "args"
-  def self.call(*args)
+  def self.call
     # This calls initialize, converting the args array back into arguments
-    self.new(*args).call
+    self.new.call
   end
 
   # Run logic here
   def call
-    # parsed_json["results"].each do |event_hash|
-    #   # Find a Campus that matches the name, or create one
-    #   campus = Campus.where(name: event_hash["campus"]).first_or_create!
-
-    #   # Create an event
-    #   Event.create!(
-    #     campus: campus,
-    #     title: event_hash["title"],
-    #     description: event_hash["description"],
-    #     url: event_hash["url"],
-    #     foreign_id: event_hash["id"],
-    #     start_at: DateTime.parse(event_hash["startdt"]),
-    #     end_at: DateTime.parse(event_hash["enddt"]),
-    #   )
-    #end
-    url_string = 'http://calendar.libraryweb.org/ajax/calendar/list?c=-1&perpage=100&page=%d&date=0000-00-00'
-    url = URI.parse('http://calendar.libraryweb.org/ajax/calendar/list?c=-1&perpage=100&page=1&date=0000-00-00')
-    req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port) {|http|
-    http.request(req)}
+    res = get_json_results
 
     JsonToEvents.call(res.body)
-    
+
     result = JSON.parse(res.body)
     total_events = result["total_results"]
 
-    puts total_events
     iterations = total_events /  100
 
-    puts iterations
-    
-    puts iterations.round
     i = 2
 
     while i <= iterations  do
-      puts("Inside the loop i = %d", i )
-
-      url = URI.parse(url_string % [i])
-      req = Net::HTTP::Get.new(url.to_s)
-      res = Net::HTTP.start(url.host, url.port) {|http|
-      http.request(req)}
+      res = get_json_results
 
       JsonToEvents.call(res.body)
 
@@ -67,7 +40,12 @@ class DataRequest
 
   private
 
-  def parsed_data
-    data
+  def get_json_results
+    req = Net::HTTP::Get.new(API_URL.to_s)
+    Net::HTTP.start(API_URL.host, API_URL.port) do |http|
+      http.request(req)
+    end
   end
+
+
 end
--- test/services/json_to_events_test.rb
+++ test/services/json_to_events_test.rb
@@ -5,15 +5,15 @@ class JsonToEventsTest < ActiveSupport::TestCase
     @raw_json = File.open(Rails.root.join("test/fixtures/files/request.json")).read
   end
 
-  # test "creates campuses" do
-  #   assert_difference "Campus.count", 14 do
-  #     JsonToEvents.call(@raw_json)
-  #   end
-  # end
+  test "creates campuses" do
+    assert_difference "Campus.count", 14 do
+      JsonToEvents.call(@raw_json)
+    end
+  end
 
-  # test "creates events" do
-  #   assert_difference "Event.count", 20 do
-  #     JsonToEvents.call(@raw_json)
-  #   end
-  # end
+  test "creates events" do
+    assert_difference "Event.count", 20 do
+      JsonToEvents.call(@raw_json)
+    end
+  end
 end
