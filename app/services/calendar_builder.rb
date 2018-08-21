3811ef2b4940148e113f5134f3e5becc8b2185ac
Kyle Macey <kylemacey@github.com>
2018-08-20 21:14:39 -0400

    Fix library ids add pry order results

--- app/services/calendar_builder.rb
+++ app/services/calendar_builder.rb
@@ -17,19 +17,19 @@ class CalendarBuilder
   # Run logic here
   def call
   	ical_cal = Icalendar::Calendar.new
-	Event.includes(:calendar).where(calendars: { foreign_id: ids }, end_at: (Date.today..(3.months.from_now))).each do |event|
-		ical_event = Icalendar::Event.new
-		ical_event.summary = event.title
-		ical_event.description = event.description
-		ical_event.dtstart = event.start_at
-		ical_event.dtend = event.end_at
-		ical_event.url = event.url
-		ical_event.location = event.campus.name
-		ical_cal.add_event(ical_event)
-	end
+    Event.includes(:calendar).where(calendars: { foreign_id: ids }, end_at: (Date.today..(3.months.from_now))).order(start_at: :asc).each do |event|
+      ical_event = Icalendar::Event.new
+      ical_event.summary = event.title
+      ical_event.description = event.description
+      ical_event.dtstart = event.start_at
+      ical_event.dtend = event.end_at
+      ical_event.url = event.url
+      ical_event.location = event.campus.name
+      ical_cal.add_event(ical_event)
+    end
 
-	ical_cal
-  
+    ical_cal
+
   end
 
 end
